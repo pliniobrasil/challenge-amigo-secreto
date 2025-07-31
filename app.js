8407c1f3e2dd18988183b4f3ee6c7de03eb7ecac
Plinio <brasil.plinio@gmail.com>
2025-07-31 12:54:17 -0300

    add funcao para listar amigos

--- app.js
+++ app.js
@@ -8,10 +8,21 @@ function adicionarAmigo() {
     } else if (nome != '') {
         nomeParticipantes.push(nome);
         limparCampo();
+        listarParticipantes();
     }
 }
 
 function limparCampo() {
     nome = document.querySelector('input');
     nome.value = '';
+}
+
+function listarParticipantes() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ''; // Limpa a lista antes de adicionar os participantes
+    nomeParticipantes.forEach(function(participante) {
+        let item = document.createElement('li');
+        item.textContent = participante;
+        lista.appendChild(item);
+    });
 }
